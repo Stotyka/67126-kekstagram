d551897b9873e545669bc1d4bc2d9606fe4992cf
Yuriy Stotyka <stotyka@gmail.com>
2016-08-08 08:39:38 +0300

    Add files via upload

--- build/js/check.js
+++ build/js/check.js
@@ -41,14 +41,14 @@ function getMessage (a, b) {
     if (Array.isArray(b)) { // проверка массив ли b
       if ( b ) {
         var artifactsSquare = 0;
-        for (var i = 0, i < a.length, i++) {
+        for (var i = 0; i < a.length; i++) {
           artifactsSquare += a[i] * b[i];
         }
       return "Общая площадь артефактов сжатия: [artifactsSquare] пикселей";
-
+      }
     } else {
       var amountOfRedPoints = 0;
-      for (var i = 0, i < a.length, i++) {
+      for (var i = 0; i < a.length; i++) {
         amountOfRedPoints += a[i];
       }
       return "Количество красных точек во всех строчках изображения: [amountOfRedPoints]";
