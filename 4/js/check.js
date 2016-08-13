f1f8ee556091aaff95399ee424b52df3002b82a9
Keks <keks@htmlacademy.ru>
2016-08-13 15:13:45 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/check.js
@@ -0,0 +1,36 @@
+function getMessage (a, b) {
+
+  if (typeof ( a ) == 'boolean') {
+    if ( a ) {
+      return "Переданное GIF-изображение анимировано и содержит " + [b] + " кадров";
+    } else {
+      return "Переданное GIF-изображение не анимировано";
+    }
+  }
+
+  if (typeof ( a ) == 'number') {
+    if ( a ) {
+      return "Переданное SVG-изображение содержит " + [a] + " объектов и " + [b * 4] + " атрибутов";
+    }
+  }
+
+  if (Array.isArray(a)) { 
+
+    if (Array.isArray(b)) { 
+      if ( b ) {
+        var artifactsSquare = 0;
+        for (var i = 0; i < a.length; i++) {
+          artifactsSquare += a[i] * b[i];
+        }
+      return "Общая площадь артефактов сжатия: " + [artifactsSquare] + " пикселей";
+      }
+    } else {
+      var amountOfRedPoints = 0;
+      for (var i = 0; i < a.length; i++) {
+        amountOfRedPoints += a[i];
+      }
+      return "Количество красных точек во всех строчках изображения: " + [amountOfRedPoints];
+    }
+  }
+}
+
